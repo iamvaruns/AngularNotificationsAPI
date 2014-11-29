dd951706060cebef907593162cb3993972763721
Varun Sheth <varun.sheth@tubemogul.com>
2014-11-28 17:10:51 -0800

    Working prototype for notifications

---
+++ js/app.js
@@ -0,0 +1,63 @@
+/**
+ * Created by varun.sheth on 11/26/14.
+ */
+(function(){
+
+    var app = angular.module('notifications', []);
+
+    app.controller('NotificationsController', ['$http', function($http){
+        var notifications = this;
+
+        notifications.userNotifications = [];
+
+        $http.get('js/notifications.json').success(function(data){
+            notifications.userNotifications = data;
+        });
+    }]);
+
+    app.directive('notificationsContainer', function(){
+       return {
+           restrict: 'E',
+           templateUrl: 'notifications-container.html',
+           controller: function(){
+               this.containerClass = 'notify';
+
+               this.setContainerClass = function(setClass){
+                   switch(setClass) {
+                       case 1: this.containerClass = "notify danger-border";
+                           break;
+                       case 2: this.containerClass = "notify info-border";
+                           break;
+                       case 3: this.containerClass = "notify warning-border";
+                           break;
+                       case 4: this.containerClass = "notify success-border";
+                           break;
+                       default: this.containerClass = "notify unknown-border";
+                           break;
+                   }
+
+                   return this.containerClass;
+               };
+
+               this.setTitleClass = function(setClass){
+                   switch(setClass) {
+                       case 1: this.titleClass = "danger";
+                           break;
+                       case 2: this.titleClass = "info";
+                           break;
+                       case 3: this.titleClass = "warning";
+                           break;
+                       case 4: this.titleClass = "success";
+                           break;
+                       default: this.titleClass = "unknown";
+                           break;
+                   }
+
+                   return this.titleClass;
+               };
+           },
+           controllerAs: 'notificationsContainer'
+       };
+    });
+
+})();
